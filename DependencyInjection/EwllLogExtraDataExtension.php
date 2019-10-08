b8d97af5650b5b15c5b2ec2620736ab752ce7675
Maxim Salamakhin <343604@gmail.com>
2019-10-08 23:16:28 +0700

    dependency injectioin

---
+++ DependencyInjection/EwllLogExtraDataExtension.php
@@ -0,0 +1,21 @@
+<?php namespace Ewll\LogExtraDataBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * {@inheritdoc}
+ */
+class EwllLogExtraDataExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
