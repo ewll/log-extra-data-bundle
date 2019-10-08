9163256c54d47633a057171d28dd2c68c1302226
Maxim Salamakhin <343604@gmail.com>
2019-10-08 17:41:47 +0700

    init

---
+++ EwllLogExtraDataBundle.php
@@ -0,0 +1,10 @@
+<?php namespace Ewll\LogExtraDataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * {@inheritdoc}
+ */
+class EwllLogExtraDataBundle extends Bundle
+{
+}
---
+++ GlobalLogProcessor.php
@@ -0,0 +1,21 @@
+<?php namespace Ewll\LogExtraDataBundle;
+
+class GlobalLogProcessor
+{
+    private $logExtraDataKeeper;
+
+    public function __construct(LogExtraDataKeeper $logExtraDataKeeper)
+    {
+        $this->logExtraDataKeeper = $logExtraDataKeeper;
+    }
+
+    public function processRecord(array $record)
+    {
+        $data = $this->logExtraDataKeeper->getData();
+        foreach ($data as $key => $value) {
+            $record['extra'][$key] = $value;
+        }
+
+        return $record;
+    }
+}
